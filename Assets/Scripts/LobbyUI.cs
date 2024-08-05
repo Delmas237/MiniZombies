0ca0c77020b8a5700a145954f2164fad803524ae
Delmas237 <danil732732@mail.ru>
2024-08-05 20:45:56 +0300

    Fixed lobby UI

---
+++ Assets/Scripts/LobbyUI.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _coinsText;
+    [SerializeField] private TextMeshProUGUI _maxWaveText;
+
+    private void Awake()
+    {
+        _maxWaveText.text = PlayerPrefs.GetInt("MaxWave").ToString();
+
+        UpdateText(Bank.Coins);
+        Bank.CoinsChanged += UpdateText;
+    }
+    private void OnDestroy()
+    {
+        Bank.CoinsChanged -= UpdateText;
+    }
+
+    private void UpdateText(int amount)
+    {
+        _coinsText.text = amount.ToString();
+    }
+
+    public void Play() => SceneManager.LoadScene("Game");
+}
