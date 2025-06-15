64bfa305cbd05282fac805bb8d0ce30465e6bb43
Delmas237 <102839766+Delmas237@users.noreply.github.com>
2025-06-15 16:46:37 +0300

    Changed GlobalShopData loading to addressables

--- Assets/Scripts/Features/GlobalShop/GlobalShop.cs
+++ Assets/Scripts/Features/GlobalShop/GlobalShop.cs
@@ -1,6 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using Weapons;
 
 namespace GlobalShopLib
@@ -8,18 +11,33 @@ namespace GlobalShopLib
     public class GlobalShop : MonoBehaviour
     {
         [SerializeField] private List<GlobalShopItem> _items;
-        [SerializeField] private string _dataPath = "Data/GlobalShopData";
+        [SerializeField] private string _dataKey = "GlobalShopData";
         private GlobalShopData _data;
 
         private void Awake()
         {
-            _data = Resources.Load<GlobalShopData>(_dataPath);
-            
+            StartCoroutine(Initialize());
+        }
+        private IEnumerator Initialize()
+        {
+            yield return StartCoroutine(LoadData());
+
             foreach (var item in _items)
             {
                 item.Intialize(_data.Items.First(i => i.Type == item.Type));
                 item.Updated += GunsDataSaver.Save;
             }
+        }
+        private IEnumerator LoadData()
+        {
+            AsyncOperationHandle<GlobalShopData> handle = Addressables.LoadAssetAsync<GlobalShopData>(_dataKey);
+
+            yield return handle;
+
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+                _data = handle.Result;
+            else
+                Debug.LogError("Failed to load data");
         }
 
         private void Update()
